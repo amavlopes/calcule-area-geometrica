63e09e3f9f24e3b6a50470fdac3881829eb8d0a6
Amanda Avelino <amavlopes@gmail.com>
2025-03-19 20:22:39 -0300

    refactor: Instancia classe Main em app.ts
    Instancia classe Main em app.ts e executa seu método estático init para cálculo das áreas geométricas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import express from "express";
+
 import routes from "./routes";
-import { Circulo } from "./app/models/Circulo";
-import { Quadrado } from "./app/models/Quadrado";
-import { Retangulo } from "./app/models/Retangulo";
-import { Triangulo } from "./app/models/Triangulo";
+import Main from "./app/index";
 
 class App {
   private _server;
@@ -21,35 +19,8 @@ class App {
     return this._server;
   }
 
-  init(): void {
-    // Circulo
-    const circulo = new Circulo(0.5);
-    circulo.calcularArea();
-    circulo.mostrarLog();
-
-    circulo.raio = 3;
-    circulo.calcularArea();
-    circulo.mostrarLog();
-
-    // Quadrado
-    const quadrado = new Quadrado(2);
-    quadrado.calcularArea();
-    quadrado.mostrarLog();
-
-    // Retangulo
-    const retangulo = new Retangulo(10, 2);
-    retangulo.calcularArea();
-    retangulo.mostrarLog();
-
-    // Triangulo
-    const triangulo = new Triangulo(20, 4);
-    triangulo.calcularArea();
-    triangulo.mostrarLog();
-
-    triangulo.base = 10;
-    triangulo.altura = 5;
-    triangulo.calcularArea();
-    triangulo.mostrarLog();
+  init() {
+    Main.init();
   }
 
   middlewares(): void {
