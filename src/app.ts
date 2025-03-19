25fd2760096855f447aa5033844f0fd5ac50fd5e
Amanda Avelino <amavlopes@gmail.com>
2025-03-19 19:49:51 -0300

    feat: Instancia as classes Circulo e Quadrado e calcula área
    Cria instâncias das classes Circulo e Quadrado e calcula suas áreas conforme forma criada

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import routes from "./routes";
+import { FormaGeometrica } from "./app/models/FormaGeometrica";
+import { Circulo } from "./app/models/Circulo";
+import { Quadrado } from "./app/models/Quadrado";
 
 class App {
   private _server;
@@ -9,17 +12,35 @@ class App {
 
     this.middlewares();
     this.routes();
+
+    this.init();
   }
 
   get server() {
     return this._server;
   }
 
-  middlewares() {
+  init(): void {
+    // Círculo
+    const circulo = new Circulo(0.5);
+    circulo.calcularArea();
+    circulo.mostrarLog();
+
+    circulo.raio = 3;
+    circulo.calcularArea();
+    circulo.mostrarLog();
+
+    // Quadrado
+    const quadrado = new Quadrado(2);
+    quadrado.calcularArea();
+    quadrado.mostrarLog();
+  }
+
+  middlewares(): void {
     this._server.use(express.json());
   }
 
-  routes() {
+  routes(): void {
     this._server.use(routes);
   }
 }
